588402177fc77941158b9e5a13561b142dc1e62c
zainab mukhtar <zainabmukhtar2277@gmail.com>
2025-08-29 18:02:38 +0500

    uv_simple_application&_uv_packaged_application

---
+++ my-packaged-app/src/my_packaged_app/main.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Hello from my packaged app! -Zainab, PIAIC258932")
+    print(25*5+300-198)
+
+if __name__ == "__main__":
+    main()    
---
+++ my-simple-app/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from my-simple-app! -Zainab, PIAIC258932")
+    print(20*400-49+2)
+
+
+if __name__ == "__main__":
+    main()
